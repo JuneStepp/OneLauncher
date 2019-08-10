a8433e92fe102c81292d0cb118cd77830d7e5715
Jeremy Stepp <jeremy@bluetecno.com>
2019-08-10 13:26:59 -0500

    1.1 release

--- OneLauncher/Information.py
+++ OneLauncher/Information.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-Version = "1.0.0"
+Version = "1.1"
 Description = "LOTRO/DDO Custom Launcher"
 Website = "<a href=\"https://github.com/JeremyStepp/OneLauncher\">Github.com/JeremyStepp/OneLauncher</a>"
 Author = "Jeremy Stepp"
