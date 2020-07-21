83c7357e1887a48c65089115d59d4623c1c7b454
June Stepp <git@junestepp.me>
2020-07-21 14:44:44 -0500

    Optimized included build packages

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from cx_Freeze import setup, Executable
 import OneLauncher.Information as info
 from platform import system
+import PySide2
+import os.path
+
 
 packages_list = [
     "OneLauncher.Information",
     "keyring.backends",
     "cryptography",
+    "PySide2.QtXml",
 ]
 
 platform = system()
@@ -13,18 +17,23 @@ platform = system()
 if platform == "Windows":
     base = "Win32GUI"
     packages_list.append("win32timezone")
+
+    plugins_path = os.path.join(PySide2.__path__[0], "plugins")
 else:
     base = ""
+    plugins_path = os.path.join(PySide2.__path__[0], "Qt", "plugins")
 
 build_options = {
     "build_exe": "build",
     "packages": packages_list,
-    "excludes": [],
+    "zip_include_packages": ["PySide2", "PySide2.QtXml", "shiboken2", "encodings"],
+    "excludes": ["tkinter", "unittest", "pydoc", "pdb"],
     "optimize": "2",
     "include_files": [
         "OneLauncher/ui",
         "OneLauncher/certificates",
         "OneLauncher/images",
+        os.path.join(plugins_path, "platforms")
     ],
 }
 
