933c137c30e80c9f6a664d9932875ffdbe1eff09
June Stepp <git@junestepp.me>
2023-01-08 12:47:50 -0600

    fix: Remove addons config references to in-dev stuff

--- src/onelauncher/config/games/addons.py
+++ src/onelauncher/config/games/addons.py
@@ -3,7 +3,6 @@ from pathlib import Path
 from typing import Any, Dict, Final
 
 from onelauncher.addons.manager import AddonsManager
-from onelauncher.addons.feed import LotroInterfaceFeed
 from onelauncher.config.games import games_config
 from onelauncher.games import Game
 
@@ -15,28 +14,13 @@ def get_addons_manager_from_game(game: Game) -> AddonsManager:
         game.uuid, CONFIG_SECTION_NAME)
     return AddonsManager(game,
                          [Path(script) for script in config.get("startup_scripts",
-                                                                [])],
-                         {LotroInterfaceFeed(feed["name"],
-                                             feed["description"],
-                                             feed["addons_type"],
-                                             feed["url"]) for feed in config.get(
-                             "user_addons_feeds", [])},
-                         config.get("override_default_addons_feeds", False)
+                                                                [])]
                          )
 
 
 def get_config_from_addons_manager(
         addons_manager: AddonsManager) -> Dict[str, Any]:
     config: Dict[str, Any] = {}
-    if addons_manager.user_addons_feeds:
-        config["override_default_addons_feeds"] = addons_manager.override_default_addons_feeds
-        config["user_addons_feeds"] = [
-            {
-                "name": feed.name,
-                "description": feed.description,
-                "addons_type": feed.addons_type,
-                "url": feed.url} for feed in addons_manager.user_addons_feeds]
-
     if addons_manager.startup_scripts:
         config["startup_scripts"] = [
             str(script) for script in addons_manager.startup_scripts]
