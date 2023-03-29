374a22f134b7e946f157d7c8e9addbbce8c221dc
June Stepp <git@junestepp.me>
2023-03-29 18:05:03 -0500

    build: Update Nuitka and compile.py
    - Include all XSD files in build
    - Fix Nuitka build command syntax
    - Update Nuitka

--- compile.py
+++ compile.py
@@ -5,24 +5,24 @@ nuitka_arguments = [
     "--assume-yes-for-downloads",
     "--nofollow-import-to=tkinter,unittest,pydoc,pdb",
     "--plugin-enable=pyside6",
-    "--include-package keyring.backends",
-    "--include-data-dir src/onelauncher/network/schemas=onelauncher/network/schemas",
-    "--include-data-dir src/onelauncher/images=onelauncher/images",
-    "--include-data-dir src/onelauncher/locale=onelauncher/locale",
-    "--include-data-dir src/onelauncher/fonts=onelauncher/fonts",
+    "--include-package=keyring.backends",
+    "--include-data-files=src/onelauncher/=onelauncher/=**/*.xsd",
+    "--include-data-dir=src/onelauncher/images=onelauncher/images",
+    "--include-data-dir=src/onelauncher/locale=onelauncher/locale",
+    "--include-data-dir=src/onelauncher/fonts=onelauncher/fonts",
     ]
 
 def main():
     if os.name == "nt":
         nuitka_arguments.extend(["--windows-disable-console",
-                                 "--include-package win32timezone",
-                                 "--include-package platformdirs.windows",
+                                 "--include-package=win32timezone",
+                                 "--include-package=platformdirs.windows",
                                  "--windows-icon-from-ico=src/onelauncher/images/OneLauncherIcon.ico"])
     elif os.name == "mac":
-        nuitka_arguments.extend(["--include-package platformdirs.macos",])
+        nuitka_arguments.extend(["--include-package=platformdirs.macos",])
     else:
         nuitka_arguments.extend(["--include-qt-plugins=wayland-shell-integration,platforms",
-                                 "--include-package platformdirs.unix",])
+                                 "--include-package=platformdirs.unix",])
 
     os.system(f"poetry run python -m nuitka {' '.join(nuitka_arguments)} start_onelauncher")
 
