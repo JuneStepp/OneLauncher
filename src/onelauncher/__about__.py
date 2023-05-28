1fd90b5c825595a2661958b0d8a85120ce879335
June Stepp <git@junestepp.me>
2023-05-28 18:30:56 -0500

    docs: Update copyright year

--- src/onelauncher/__about__.py
+++ src/onelauncher/__about__.py
@@ -7,7 +7,7 @@ __project_url__ = "https://GitHub.com/JuneStepp/OneLauncher"
 __author__ = "June Stepp"
 __author_email__ = "contact@JuneStepp.me"
 __license__ = "GPL-3.0-or-later"
-__copyright__ = "(C) 2019-2021 June Stepp"
+__copyright__ = "(C) 2019-2023 June Stepp"
 __copyright_history__ = ("Based on PyLotRO\n(C) 2009-2010 AJackson\n"
                          "Based on CLI launcher for LOTRO\n(C) 2007-2010 SNy\n"
                          "Based on CLI launcher for LOTRO\n(C) 2007-2010 SNy")
