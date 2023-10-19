15b26aefbaf4abc125c33cdffa91fb76263040f0
June Stepp <git@junestepp.me>
2023-10-18 23:53:03 -0500

    feat: Enable ensure_exists in platformdirs

--- src/onelauncher/config/__init__.py
+++ src/onelauncher/config/__init__.py
@@ -2,4 +2,4 @@ from platformdirs import PlatformDirs
 
 from ..__about__ import __title__
 
-platform_dirs = PlatformDirs(__title__.lower(), False)
+platform_dirs = PlatformDirs(__title__.lower(), ensure_exists=True)
