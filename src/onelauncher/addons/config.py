edc7bee84e4d241b2ab20ce5df825d6d9c020608
June Stepp <git@junestepp.me>
2024-04-28 17:26:01 -0500

    fix: Add config field for enabled_startup_scripts
    Signed-off-by: June Stepp <git@junestepp.me>

--- src/onelauncher/addons/config.py
+++ src/onelauncher/addons/config.py
@@ -1,5 +1,3 @@
-from pathlib import Path
-
 import attrs
 
 from ..config import config_field
@@ -7,7 +5,8 @@ from .startup_script import StartupScript
 
 
 @attrs.frozen
-class AddonsConfigSection():
+class AddonsConfigSection:
     enabled_startup_scripts: tuple[StartupScript, ...] = config_field(
-        default=(), help="Startup scripts run before game launch"
+        default=(),
+        help="Python scripts run before game launch. Paths are relative to the game's documents config directory",
     )
